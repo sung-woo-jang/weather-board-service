7292bd96d20f1adf54132a14752b0c1d81cec97d
gyomdyung <seastory624@gmail.com>
2022-09-06 17:49:57 +0900

    Chore: docker-compose 파일 추가

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,12 +3,18 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const typeOrmAsyncModuleOptions = {
   useFactory: async (): Promise<TypeOrmModuleOptions> => ({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'test',
+    database: 'test',
     namingStrategy: new SnakeNamingStrategy(),
-    type: 'sqlite',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    database: 'database.db',
+    // database: 'database.db',
     synchronize: process.env.MODE == 'dev', //! set 'false' in production
     autoLoadEntities: true,
     logging: process.env.MODE == 'dev',
+    charset: 'utf8mb4_unicode_ci',
   }),
 };
